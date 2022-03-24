b8de9cc0115ee4b2b4d742a2adfdf1d340f25e64
nafsuki <48878455+Nafsuki@users.noreply.github.com>
2022-03-24 13:47:23 +0100

    Create MemoEditScreen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    // <MemoListScreen />
     // <MemoDetailScreen />
+    <MemoEditScreen />
   );
 }
 
